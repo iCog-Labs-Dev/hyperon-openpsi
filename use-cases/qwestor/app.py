5527cfc8b8f155e8a23b379d77238bc82834a14b
Nahom32 <senaynahom00@gmail.com>
2025-10-17 10:17:26 +0300

    fix: added robust try catch blocks to the api endpoints

--- use-cases/qwestor/app.py
+++ use-cases/qwestor/app.py
@@ -11,60 +11,100 @@ def root():
 
 @app.post("/plan")
 async def plan():
-    process = await asyncio.create_subprocess_shell(
-        "metta main-loop.metta",
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
-    )
+    try:
+      
+        process = await asyncio.create_subprocess_shell(
+            "metta main-loop.metta",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
 
-    stdout, stderr = await process.communicate()
+        stdout, stderr = await process.communicate()
 
-    if process.returncode != 0:
-        return {"error": f"metta failed with code {process.returncode}", "stderr": stderr.decode()}
-    result = stdout.decode().strip()
-    refinedOutput = preprocessRawOutput(result)
-    actions_expression = refinedOutput[-1]
-    json_result = {"actions": changeSexpToList(actions_expression)}
-    await asyncio.to_thread(writeListToFile, refinedOutput, "out.metta")
+        
+        if process.returncode != 0:
+            return {
+                "error": f"metta failed with code {process.returncode}",
+                "stderr": stderr.decode().strip()
+            }
 
-    return json_result
+        try:
+            result = stdout.decode().strip()
+            refinedOutput = preprocessRawOutput(result)
+            actions_expression = refinedOutput[-1]
+            json_result = {"actions": changeSexpToList(actions_expression)}
+
+           
+            await asyncio.to_thread(writeListToFile, refinedOutput, "out.metta")
+
+            return json_result
+
+        except Exception as e:
+            return {"error": f"Processing error: {e}"}
+
+    except asyncio.SubprocessError as e:
+        return {"error": f"Async subprocess failed: {e}"}
+
+    except Exception as e:
+        return {"error": f"Unexpected server error: {e}"}
 
 
 @app.get("/fetchModulators")
 async def fetchModulators():
-    process = await asyncio.create_subprocess_shell(
-        "metta fetch-modulators.metta",
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
-    )
+    try:
+        process = await asyncio.create_subprocess_shell(
+            "metta fetch-modulators.metta",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
 
-    stdout, stderr = await process.communicate()
+        stdout, stderr = await process.communicate()
 
-    if process.returncode != 0:
-        return {"error": f"metta failed with code {process.returncode}", "stderr": stderr.decode()}
-    result = stdout.decode().strip()
-    refinedOutput = preprocessRawOutput(result)
-    modulatorSexp = refinedOutput[-1]
-    return {"modulators": preprocessMechanicsOutput(modulatorSexp, "modulator")}
+        if process.returncode != 0:
+            return {"error": f"metta failed with code {process.returncode}", "stderr": stderr.decode()}
+        try:
+            result = stdout.decode().strip()
+            refinedOutput = preprocessRawOutput(result)
+            modulatorSexp = refinedOutput[-1]
+            return {"modulators": preprocessMechanicsOutput(modulatorSexp, "modulator")}
+        except Exception as e:
+            return {"error": f"processing Error {e}"}
+    except asyncio.SubprocessError as e:
+        return {"error": f"Async subprocess failed: {e}"}
+
+    except Exception as e:
+        return {"error": f"Unexpected server error: {e}"}
+    
+
 
 
 
 @app.get("/fetchDemands")
 async def fetchDemands():
-    process = await asyncio.create_subprocess_shell(
-        "metta fetch-demands.metta",
-        stdout=asyncio.subprocess.PIPE,
-        stderr=asyncio.subprocess.PIPE
-    )
+    try:
+        process = await asyncio.create_subprocess_shell(
+            "metta fetch-demands.metta",
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE
+        )
 
-    stdout, stderr = await process.communicate()
+        stdout, stderr = await process.communicate()
 
-    if process.returncode != 0:
-        return {"error": f"metta failed with code {process.returncode}", "stderr": stderr.decode()}
-    result = stdout.decode().strip()
-    refinedOutput = preprocessRawOutput(result)
-    demandSexp = refinedOutput[-1]
-    return {"demands": preprocessMechanicsOutput(demandSexp, "demand")}
+        if process.returncode != 0:
+            return {"error": f"metta failed with code {process.returncode}", "stderr": stderr.decode()}
+        try: 
+            result = stdout.decode().strip()
+            refinedOutput = preprocessRawOutput(result)
+            demandSexp = refinedOutput[-1]
+            return {"demands": preprocessMechanicsOutput(demandSexp, "demand")}
+        except Exception as e:
+            return {"error": f"processing error {e}"}
+    except asyncio.SubprocessError as e:
+        return {"error": f"Async subprocess failed: {e}"}
+
+    except Exception as e:
+        return {"error": f"Unexpected server error: {e}"}
+    
 
 
 
