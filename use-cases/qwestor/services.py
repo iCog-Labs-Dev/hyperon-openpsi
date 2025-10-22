fa00028c36b06dfc13d36609fc687757b7529fc8
samwondim <samwondim@gmail.com>
2025-10-22 13:57:16 +0300

    modified rule output string formatter.

--- use-cases/qwestor/services.py
+++ use-cases/qwestor/services.py
@@ -9,9 +9,9 @@ def format_rules(rules: List[RuleCreate]) -> str:
     output = ""
 
     for i, rule in enumerate(rules):
-        output += """
-            ((: r{id} ((TTV {id} (STV 0.8 0.7)) (IMPLICATION_LINK (AND_LINK ((Goal {context} 0.9 0.6) {action})) (Goal {goal} 1.0 1.0)))) {id})
-        """.format(id=i, context=rule.context, action=rule.action, goal=rule.goal)
+        output += """((: r{id} ((TTV {id} (STV 0.8 0.7)) (IMPLICATION_LINK (AND_LINK ((Goal {context} 0.9 0.6) {action})) (Goal {goal} 1.0 1.0)))) {id})\n""".format(
+            id=i, context=rule.context, action=rule.action, goal=rule.goal
+        )
 
     return output
 
